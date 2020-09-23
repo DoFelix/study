eead34aa82473197c5b56cd1dba142bc7e2895ef
DoFelix <1390095964@qq.com>
2020-09-23 14:28:17 +0800

    把maven打包问题解决了

--- demo1/src/main/java/com/HelloWorldApplication.java
+++ demo1/src/main/java/com/HelloWorldApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//标注一个主程序 说明这是一个spingboot应用
+// 标注一个主程序 说明这是一个spingboot应用
 public class HelloWorldApplication {
     public static void main(String[] args) {
         //应用启动起来
         SpringApplication.run(HelloWorldApplication.class,args);
+        System.out.println("启动成功");
     }
 }
